ca5f2b67cd799cdbd5714683d304d50f4cd5c298
gengsmb88@gmail.com <gengsmb88@gmail.com>
2022-02-20 20:03:55 +0700

    post_params

--- src/Id/Linkaja/Linkaja.php
+++ src/Id/Linkaja/Linkaja.php
@@ -43,10 +43,10 @@ class Linkaja {
 	
     private $authToken, $uniqueId, $sessionId, $pin, $idKey;
 	private static $transfer_action_mark = [
-		'ovo'			=> 'trf_ovo',
+		'ovo'			=> 'trf_linkaja',
 		'bank'			=> 'trf_other_bank',
-		'cash_ovo'		=> 'OVO Cash',
-		'cash_bank'		=> 'OVO Cash',
+		'cash_wallet'	=> 'Linkaja Cash',
+		'cash_bank'		=> 'Linkaja Cash',
 	];
 	private static $transfer_minimum = [
 		'wallet'		=> 10000,
@@ -112,11 +112,178 @@ class Linkaja {
 		$this->headers['Client-Id'] = sprintf("%s", self::client_id);
 		return $this->headers;
 	}
-
+	
+	//----
+	// CURL Instances
+	//----
+	private function set_curl_init($url_hostname, $headers = null) {
+		$this->ch = curl_init();
+		curl_setopt($this->ch, CURLOPT_URL, $url_hostname);
+		curl_setopt($this->ch, CURLOPT_VERBOSE, TRUE);
+		//curl_setopt($this->ch, CURLOPT_COOKIELIST, TRUE);
+		//curl_setopt($this->ch, CURLOPT_FAILONERROR, TRUE);
+		curl_setopt($this->ch, CURLOPT_SSL_VERIFYHOST, 2);
+		curl_setopt($this->ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+		curl_setopt($this->ch, CURLOPT_SSLVERSION, 6);
+		curl_setopt($this->ch, CURLOPT_COOKIESESSION, TRUE);
+		
+		//curl_setopt($this->ch, CURLOPT_COOKIEFILE, ($this->cookies_path . DIRECTORY_SEPARATOR . "{$this->acc_num}.log"));
+		//curl_setopt($this->ch, CURLOPT_COOKIEJAR, ($this->cookies_path . DIRECTORY_SEPARATOR . "{$this->acc_num}.log"));
+		curl_setopt($this->ch, CURLOPT_HEADER, FALSE);
+		if (isset($headers)) {
+			curl_setopt($this->ch, CURLOPT_HTTPHEADER, $headers);
+		}
+		curl_setopt($this->ch, CURLOPT_CONNECTTIMEOUT, $this->curl_options['timedout_connect']);
+		curl_setopt($this->ch, CURLOPT_TIMEOUT, $this->curl_options['timedout_execution']);
+		curl_setopt($this->ch, CURLOPT_USERAGENT, $this->curl_options['user_agent']);
+		curl_setopt($this->ch, CURLOPT_ENCODING, "");
+		curl_setopt($this->ch, CURLOPT_FOLLOWLOCATION, true);
+		curl_setopt($this->ch, CURLOPT_AUTOREFERER, true);
+		
+		return $this->ch;
+	}
+	private function curlexec() {
+        curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, TRUE);
+		$this->curl_collect['error_code'] = 0;
+		$this->curl_collect['error_msg'] = '';
+		
+		try {
+			$this->curl_collect['http_body'] = curl_exec($this->ch);
+		} catch (Exception $ex) {
+			throw $ex;
+			$this->curl_collect['http_body'] = FALSE;
+		}
+		$this->curl_collect['http_code'] = curl_getinfo($this->ch, CURLINFO_HTTP_CODE);
+		if (curl_error($this->ch)) {
+			$this->curl_collect['error_code'] = curl_errno($this->ch);
+			$this->curl_collect['error_msg'] = curl_error($this->ch);
+		}
+		curl_close($this->ch);
+		
+        return $this->curl_collect;
+    }
+	public function generate_curl_headers($headers = null) {
+		if (!isset($headers)) {
+			$headers = $this->headers;
+		}
+		return $this->create_curl_headers($headers);
+	}
+	private function create_curl_headers($headers = array()) {
+		$headers_curl = array();
+		if (is_array($headers) && (count($headers) > 0)) {
+			foreach ($headers as $hkey => $hval) {
+				$headers_curl[] = sprintf("%s: %s", $hkey, $hval);
+			}
+		}
+		return $headers_curl;
+	}
+	private function set_headers($headers = array()) {
+		$this->headers = $headers;
+		return $this;
+	}
+	public function reset_headers() {
+		$this->headers = null;
+		return $this;
+	}
+	public function get_curl_instance_created() {
+		return $this->curl;
+	}
 	//-----------------------------------------------------------------------------------------------------------------------
 	// Actions:
 	//-----------------------------------------------------------------------------------------------------------------------
 	// Informasi
+	// Get Auth For First Time
+	public function get_informasi_auth(String $otp_pin = '') {
+		if (empty($otp_pin)) {
+			return false;
+		}
+		if (!is_numeric($otp_pin)) {
+			return false;
+		}
+		$this->headers['x-uniqueid'] = $this->uniqueId;
+		$this->headers['x-session-id'] = $this->sessionId;
+		
+		$url_api = sprintf("%s/%s?msisdn=%s&pin=&s", 
+			self::api_url, 
+			'otp',
+			$this->acc_num,
+			$otp_pin
+		);
+		$this->set_curl_init($url_api, $this->create_curl_headers($this->headers));
+		try {
+			$http_data = $this->call_linkaja_gateway_server('GET', $url_api, []);
+		} catch (Exception $ex) {
+			throw $ex;
+		}
+		return $http_data;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	private function call_linkaja_gateway_server(String $method, String $url, Array $post_params = []) {
+		$method = strtoupper($method);
+		if (!in_array($method, ['GET', 'POST'])) {
+			$method = 'GET';
+		}
+		
+		curl_setopt($this->ch, CURLOPT_URL, $url);
+		curl_setopt($this->ch, CURLOPT_REFERER, $url);
+		curl_setopt($this->ch, CURLOPT_CUSTOMREQUEST, $method);
+		if ($method === 'POST') {
+			curl_setopt($this->ch, CURLOPT_POST, TRUE);
+			if (!empty($post_params)) {
+				curl_setopt($this->ch, CURLOPT_POSTFIELDS, http_build_query($post_params));
+			} else {
+				curl_setopt($this->ch, CURLOPT_POSTFIELDS, '');
+			}
+		} else {
+			curl_setopt($this->ch, CURLOPT_POST, FALSE);
+			curl_setopt($this->ch, CURLOPT_HTTPGET, TRUE);
+		}
+		try {
+			$curl_collect = $this->curlexec();
+		} catch (Exception $ex) {
+			throw $ex;
+		}
+		if (isset($curl_collect['http_body'])) {
+			$http_body = json_decode($curl_collect['http_body']);
+			
+			return $http_body;
+		}
+	}
+	
+	
+	
+	
 	
 	
 	
